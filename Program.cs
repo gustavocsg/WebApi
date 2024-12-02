646b0318e5a57905447811841ed658d812badd23
gustavocsg <gustavo.sena96@gmail.com>
2024-12-01 22:23:52 -0300

    Inclusão de CORS na API. Permitindo acesso da API e os recursos disponibilizados por meio de outros domínios. Dessa forma foi configurada uma política de acesso personalizada, nomeada como 'MyPolicy', permitindo alterar ou acrescentar os acessos a qualquer momento.

--- Program.cs
+++ Program.cs
@@ -66,6 +66,17 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyPolicy",
+        policy =>
+        {
+            policy.WithOrigins("https://google.com.br", "https://youtube.com")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 var key = Encoding.ASCII.GetBytes(WebApi.Key.Secret);
 
 builder.Services.AddAuthentication(x => 
@@ -107,6 +118,8 @@ else
 {
     app.UseExceptionHandler("/error");
 }
+
+app.UseCors("MyPolicy");
 
 app.UseHttpsRedirection();
 
